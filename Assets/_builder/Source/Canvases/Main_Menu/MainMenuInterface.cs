5bd390bdc57e3cb8f7d09d8d899419d542d67609
Paweł Olszak <pawelolszak.00@gmail.com>
2026-05-01 03:59:05 +0200

    feat(ui): add boilerplate for pause and main menus

---
+++ Assets/_builder/Source/Canvases/Main_Menu/MainMenuInterface.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace nobodyworks.builder
+{
+    public class MainMenuInterface : InterfaceManager
+    {
+        [SerializeField]
+        private Button _playButton;
+
+        protected override void OnStarted()
+        {
+            _playButton.onClick.AddListener(PlayButtonClickedHandler);
+        }
+
+        private void PlayButtonClickedHandler()
+        {
+            SessionManager.Instance.CreateSession();
+        }
+    }
+}
---
+++ Assets/_builder/Source/Canvases/Pause_Menu/PauseMenuInterface.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace nobodyworks.builder
+{
+    public class PauseMenuInterface : InterfaceManager
+    {
+        [SerializeField]
+        private Button _exitButton;
+
+        protected override void OnStarted()
+        {
+            _exitButton.onClick.AddListener(ExitButtonClickedHandler);
+        }
+
+        private void ExitButtonClickedHandler()
+        {
+            SessionManager.Instance.DestroySession();
+        }
+    }
+}
