0219afef21177673c92cb85471e256713d824656
Paweł Olszak <pawelolszak.00@gmail.com>
2026-05-01 19:01:50 +0200

    feat(input): add interface for input provider, generate input actions

---
+++ Assets/_builder/Source/Input/IInputProvider.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace nobodyworks.builder.input
+{
+    public interface IInputProvider
+    {
+        Vector2 GetMove();
+        Vector2 GetLook();
+    }
+}
---
+++ Assets/_builder/Source/Input/PlayerInputProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace nobodyworks.builder.input
+{
+    public class PlayerInputProvider : IInputProvider
+    {
+        private readonly InputSystem_Actions _actionAsset = new();
+
+        public PlayerInputProvider()
+        {
+            _actionAsset.Enable();
+        }
+
+        public Vector2 GetMove()
+        {
+            return _actionAsset.Player.Move.ReadValue<Vector2>();
+        }
+        
+        public Vector2 GetLook()
+        {
+            return _actionAsset.Player.Look.ReadValue<Vector2>();
+        }
+    }
+}
