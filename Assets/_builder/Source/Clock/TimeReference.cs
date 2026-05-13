8dfec4363d5b08685b96456bbb4b326099af6956
Paweł Olszak <pawelolszak.00@gmail.com>
2026-05-13 15:44:18 +0200

    feat(clock): add TimeReference for time serialization

---
+++ Assets/_builder/Source/Clock/TimeReference.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using TriInspector;
+
+namespace nobodyworks.builder.clock
+{
+    [Serializable]
+    public class TimeReference
+    {
+        private const int SECONDS_PER_DAY = 86400;
+        private const int SECONDS_PER_HOUR = 3600;
+
+        #region Inspector
+
+        [SerializeField, Group("time")]
+        protected TimeUnits _units = TimeUnits.WithoutDay;
+
+        [SerializeField, Min(0)]
+        [ShowIf(nameof(EnabledDayUnit)), Group("time")]
+        protected int _days;
+
+        [SerializeField, Min(0)]
+        [ShowIf(nameof(EnabledHourUnit)), Group("time")]
+        protected int _hours;
+
+        [SerializeField, Min(0)]
+        [ShowIf(nameof(EnabledMinuteUnit)), Group("time")]
+        protected int _minutes;
+
+        [SerializeField, Min(0)]
+        [ShowIf(nameof(EnabledSecondUnit)), Group("time")]
+        protected int _seconds;
+
+        protected bool EnabledDayUnit => _units.HasFlag(TimeUnits.Day);
+        protected bool EnabledHourUnit => _units.HasFlag(TimeUnits.Hour);
+        protected bool EnabledMinuteUnit => _units.HasFlag(TimeUnits.Minute);
+        protected bool EnabledSecondUnit => _units.HasFlag(TimeUnits.Second);
+
+        #endregion
+
+        public int Days => _days;
+        public int Hours => _hours;
+        public int Minutes => _minutes;
+        public int Seconds => _seconds;
+
+        public TimeReference()
+        {
+            
+        }
+
+        public TimeReference(TimeUnits units, int days, int hours, int minutes, int seconds)
+        {
+            _units = units;
+            _days = days;
+            _hours = hours;
+            _minutes = minutes;
+            _seconds = seconds;
+        }
+
+        public int AsSeconds()
+        {
+            var total = 0;
+            if (EnabledDayUnit)    total += _days * SECONDS_PER_DAY;
+            if (EnabledHourUnit)   total += _hours * SECONDS_PER_HOUR;
+            if (EnabledMinuteUnit) total += _minutes * 60;
+            if (EnabledSecondUnit) total += _seconds;
+            return total;
+        }
+
+        public int AsMinutes()
+        {
+            return AsSeconds() / 60;
+        }
+
+        public int AsHours()
+        {
+            return AsSeconds() / 3600;
+        }
+    }
+}
---
+++ Assets/_builder/Source/Clock/TimeUnits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace nobodyworks.builder.clock
+{
+    [Flags]
+    public enum TimeUnits : byte
+    {
+        None = 0,
+        Day = 1,
+        Hour = 2,
+        Minute = 4,
+        Second = 8,
+
+        All = Day | Hour | Minute | Second,
+        WithoutDay = Hour | Minute | Second,
+    };
+}
